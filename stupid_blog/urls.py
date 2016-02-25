6521187423723c9b98ee59c59ce5499fa7276ad8
Iacopo Spalletti <i.spalletti@nephila.it>
2016-02-25 17:49:55 +0100

    Add translated URL

--- stupid_blog/urls.py
+++ stupid_blog/urls.py
@@ -2,10 +2,12 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
 
 from .views import PostDetailView, PostListView
 
 urlpatterns = [
     url(r'^$', PostListView.as_view(), name='posts-latest'),
-    url(r'^(?P<slug>\w[-\w]*)/$', PostDetailView.as_view(), name='post-detail'),
+    # URLs can be translated as well
+    url(_(r'^post/(?P<slug>[\w-]+)/$'), PostDetailView.as_view(), name='post-detail'),
 ]
