bf2100eab2ef79d701b8e0c2a3df5254894c9d01
Iacopo Spalletti <i.spalletti@nephila.it>
2016-09-15 13:26:22 +0200

    Add auto setup

--- stupid_blog/cms_apps.py
+++ stupid_blog/cms_apps.py
@@ -2,7 +2,9 @@
 from aldryn_apphooks_config.app_base import CMSConfigApp
 from cms.apphook_pool import apphook_pool
 from django.utils.translation import ugettext_lazy as _
+from djangocms_apphook_setup.base import AutoCMSAppMixin
 
+from .settings import get_setting
 from .cms_appconfig import BlogConfig
 
 
@@ -10,9 +12,25 @@ from .cms_appconfig import BlogConfig
 # Adding urls here will spare you from adding it in global urlconf
 # and allows to dmove the application across the page tree
 # Uses a different base class
-class StupidAppHook(CMSConfigApp):
+# Adds the auto setup mixin
+class StupidAppHook(AutoCMSAppMixin, CMSConfigApp):
     name = _('Blog')
     urls = ['stupid_blog.urls']
     app_name = 'djangocms_blog'
     app_config = BlogConfig
+    # auto_setup contains configuration values
+    # for auto creation of the missing items
+    auto_setup = {
+        'enabled': get_setting('AUTO_SETUP'),
+        'home title': get_setting('AUTO_HOME_TITLE'),
+        'page title': get_setting('AUTO_BLOG_TITLE'),
+        'namespace': get_setting('AUTO_NAMESPACE'),
+        'config_fields': {},
+        'config_translated_fields': {
+            'app_title': get_setting('AUTO_APP_TITLE'),
+            'object_name': get_setting('DEFAULT_OBJECT_NAME')
+        },
+    }
 apphook_pool.register(StupidAppHook)
+# This is the call to the setup method
+StupidAppHook.setup()
--- stupid_blog/settings.py
+++ stupid_blog/settings.py
@@ -6,6 +6,12 @@ def get_setting(config):
     # This is a method that allows easier patching during tests as settings are not evaluated on import
     # but really at runtime
     defaults = {
-        'PAGINATION': getattr(settings, 'STUPID_PAGINATION', 50)
+        'PAGINATION': getattr(settings, 'STUPID_PAGINATION', 50),
+        'AUTO_SETUP': getattr(settings, 'STUPID_AUTO_SETUP', True),
+        'AUTO_HOME_TITLE': getattr(settings, 'STUPID_AUTO_HOME_TITLE', 'Home'),
+        'AUTO_BLOG_TITLE': getattr(settings, 'STUPID_AUTO_BLOG_TITLE', 'Blog'),
+        'AUTO_NAMESPACE': getattr(settings, 'STUPID_AUTO_NAMESPACE', 'blog'),
+        'AUTO_APP_TITLE': getattr(settings, 'STUPID_AUTO_APP_TITLE', 'Blog'),
+        'DEFAULT_OBJECT_NAME': getattr(settings, 'STUPID_DEFAULT_OBJECT_NAME', 'Post'),
     }
     return defaults[config]
